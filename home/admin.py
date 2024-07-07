66d3dd9fa525697963575e4fcebfb603538eb2fb
p-sai-gowtham <puvvulasaigowtham@gmail.com>
2024-07-07 21:09:15 +0530

    remove project

--- home/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Problem_Statement, Solution
-
-# Register your models here.
-
-admin.site.register(Problem_Statement)
-admin.site.register(Solution)
